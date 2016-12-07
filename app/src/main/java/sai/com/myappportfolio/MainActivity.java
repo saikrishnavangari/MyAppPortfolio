c5049df1c77d4b23a29ed860a48ae44c9f2e0453
saikrishnavangari <saikrishna.vangari@appscore.com.au>
2016-12-07 11:56:04 +1100

    project comleted

---
+++ app/src/main/java/sai/com/myappportfolio/MainActivity.java
@@ -0,0 +1,71 @@
+package sai.com.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.Popular_Movies)
+    Button Popular_Movies;
+    @BindView(R.id.Stock_Hawk)
+    Button Stock_Hawk;
+    @BindView(R.id.Build_it_Bigger)
+    Button Build_it_Bigger;
+    @BindView(R.id.MYAM)
+    Button MYAM;
+    @BindView(R.id.Go_Ubiquitous)
+    Button Go_Ubiquitous;
+    @BindView(R.id.Capstone)
+    Button Capstone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         ButterKnife.bind(this);
+        Popular_Movies.setOnClickListener(this);
+        Stock_Hawk.setOnClickListener(this);
+        Build_it_Bigger.setOnClickListener(this);
+        MYAM.setOnClickListener(this);
+        Go_Ubiquitous.setOnClickListener(this);
+        Capstone.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        int id=view.getId();
+        switch (id){
+            case R.id.Popular_Movies:
+                displayMessage("Popular_Movies");
+                break;
+            case R.id.Stock_Hawk:
+                displayMessage("Stock_Hawk");
+                break;
+            case R.id.Build_it_Bigger:
+                displayMessage("Build_it_Bigger");
+                break;
+            case R.id.MYAM:
+                displayMessage("MYAM");
+                break;
+            case R.id.Go_Ubiquitous:
+                displayMessage("Go_Ubiquitous");
+                break;
+            case R.id.Capstone:
+                displayMessage("Capstone");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void displayMessage(String message) {
+        Toast.makeText(this,"this button will launch my "+message+"project",Toast.LENGTH_LONG).show();
+    }
+}
